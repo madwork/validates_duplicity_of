cddab2cfeb7f586156a2a263812a4f8f1697c11d
Vincent Durand <vincent.durand@madwork.org>
2013-10-17 14:52:04 +0200

    Get and set attribute directly to improve performance, and remove useless variables

--- lib/validates_duplicity_of.rb
+++ lib/validates_duplicity_of.rb
@@ -4,14 +4,12 @@ require 'active_record'
 module ValidatesDuplicityOf
   def validates_duplicity_of(attr_name, scope: nil)
     before_save do
-      return unless send "#{attr_name}_changed?"
-      attr_value = read_attribute(attr_name)
-      condition = Hash[*[attr_name].zip([attr_value]).flatten]
-      if self.class.exists?(condition)
-        names = self.class.where(["#{attr_name} LIKE ?", "#{attr_value} (%)"]).pluck(:name)
+      return unless changed.include? attr_name.to_s
+      if self.class.exists?(Hash[*[attr_name].zip([self[attr_name]]).flatten])
+        names = self.class.where(self.class.arel_table[attr_name].matches("#{self[attr_name]} (%)")).pluck(:name)
         name_ids = names.map{ |name| name.match(/\((\d+)\)$/)[1].to_i }
         index = name_ids.sort.last.to_i + 1
-        send "#{attr_name}=", "#{attr_value} (#{index})"
+        self[attr_name].concat " (#{index})"
       end
     end
   end
